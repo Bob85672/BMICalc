2fe3800624a4cabd9fc2b72f2fad4815ed777a2f
Dave Bergstein <DBergste@mathworks.com>
2019-08-08 17:39:45 -0400

    Simplifying code

--- calcBMI.m
+++ calcBMI.m
@@ -3,14 +3,5 @@ function bmi = calcBMI(weight, height)
 
 bmi = 703*weight./height^2;
 
-% Bob added code to report weight
-if bmi < 18.5
-    disp('Underweight')
-elseif bmi < 25
-    disp('Normal weight')
-else
-    disp('Overweight')
-end
-
 end
 
